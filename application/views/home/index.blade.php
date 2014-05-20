3d3f4c82849a391f2b15586f90daebd791e88294
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-05-20 12:08:49 +0100

    experimenting with blade templating engine

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -1,19 +1,5 @@
-<!doctype html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title>Laravel: A Framework For Web Artisans</title>
-	<meta name="viewport" content="width=device-width">
-	{{ HTML::style('laravel/css/style.css') }}
-</head>
-<body>
-	<!--Hello --><?php /*echo $greeting; */?>
+@layout('master')
+	@section('main')
+        here is the main content section
+    @endsection
 
-    <?php /*echo $greeting. '' .$thing; */?>
-
-    @foreach($items as $item)
-        The item is {{ $item }}
-    @endforeach
-</body>
-</html>
